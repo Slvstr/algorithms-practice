f75c926b7558b805827fb8c7d90e52d419f1337c
Erik Hellenbrand <ehellenbrand@gmail.com>
2015-02-22 19:44:52 -0600

    Generate graph and reversedGraph from scc/graph.js

---
+++ scc/findSCCs.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  var graph = require('./graph').Graph;
+  var reversedGraph = require('./graph').ReversedGraph;
+
+console.log(Object.keys(graph).length);
+console.log(Object.keys(reversedGraph).length);
+
+
+
+
+
+
+
+})();
--- scc/graph.js
+++ scc/graph.js
@@ -1,5 +1,7 @@
+
 (function() {
   'use strict';
+  var _ = require('lodash');
 
 /******************************************************************************
  * Graph class will just be a simple JS object (dictionary) mapping node names
@@ -9,26 +11,57 @@
 
   };
 
-  var fs = require('fs');
 
-  var inputText = fs.readFileSync(__dirname + '/kargerMinCut.txt', {encoding: 'utf8'});
+/******************************************************************************
+ * Load the graph from the text file and generate the original graph
+ *****************************************************************************/
+  var fs = require('fs');
+  var originalGraph = new Graph();
+  var reversedGraph;
+
+  var inputText = fs.readFileSync(__dirname + '/edges.txt', {encoding: 'utf8'});
   var inputArray = inputText.split('\n');
   inputArray.forEach(function(line) {
-    line = line.trim();
-    var lineArr = line.split('\t');
-    var nodeName = lineArr.shift();
-    edgeCount += lineArr.length;
-    var node = baseGraph.getNode(nodeName);
-    
-    lineArr.forEach(function(edgeNodeName) {
-      var edgeNode = baseGraph.getNode(edgeNodeName);
-      var edge = new Edge(node, edgeNode);
-      baseGraph.addEdge(edge);
-    });
+    var edge = line.trim().split(' ');
+    var from = edge[0];
+    var to = edge[1];
+
+    // Every node gets a key, even if it has no neighbors
+    originalGraph[from] = (originalGraph[from] ? originalGraph[from] : []);
+    originalGraph[to] = (originalGraph[to] ? originalGraph[to] : []);
+
+    originalGraph[from].push(to);
 
   });
 
+/******************************************************************************
+ * Generate reversed graph
+ *****************************************************************************/
+
+reversedGraph = new Graph();
 
 
+_.forIn(originalGraph, function(neighbors, node) {
+  if (!Array.isArray(neighbors)) console.log('unexpected key in originalGraph : ' + node);
+
+  reversedGraph[node] = reversedGraph[node] || [];
+
+  // Add node to reversed graph neighbor list of each of it's original neighbors
+  if (!neighbors.length) return;
+  neighbors.forEach(function(neighbor) {
+    reversedGraph[neighbor] = reversedGraph[neighbor] || [];
+    reversedGraph[neighbor].push(node);
+  });
+});
+
+
+/******************************************************************************
+ * Expose the original and reversed graphs
+ *****************************************************************************/
+module.exports = {
+  Graph: originalGraph,
+  ReversedGraph: reversedGraph
+};
 
 })();
+
